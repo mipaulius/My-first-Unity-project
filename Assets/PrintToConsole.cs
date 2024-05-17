8b84054afcc61fbf2d9263c44a91912b8d26a598
mipaulius <mi.paulius@gmail.com>
2024-05-17 20:18:50 +0300

    MyFirstUnityProject

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         Debug.Log("Humans, I will eat you all alive.....BOOOOOO!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
