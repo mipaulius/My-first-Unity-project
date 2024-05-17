91fe94ea18a5919d9af0641f07451c85c4899f2e
mipaulius <mi.paulius@gmail.com>
2024-05-17 20:24:13 +0300

    MyFirstUnityProject

--- Assets/PrintToConsole.cs
+++ Assets/PrintToConsole.cs
@@ -7,7 +7,7 @@ public class PrintToConsole : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         Debug.Log("Humans, I will eat you all alive.....BOOOOOO!!");
+         Debug.Log("Humans, I will eat you all alive.....BOOOOOOO!!");
     }
 
     // Update is called once per frame
